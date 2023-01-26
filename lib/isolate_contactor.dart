78e50fe5ef1a9b93d8c05195808005bc8466ab0f
Vursin <contact@vursin.com>
2023-01-26 15:12:51 +0700

    export functions

--- lib/isolate_contactor.dart
+++ lib/isolate_contactor.dart
@@ -2,4 +2,5 @@ library isolate_contactor;
 
 export 'src/isolate_contactor.dart';
 export 'src/isolate_contactor_controller.dart';
+export 'src/utils/exception.dart';
 export 'src/utils/utils.dart' show ComputeState;
